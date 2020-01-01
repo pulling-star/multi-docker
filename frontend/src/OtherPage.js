657775c6040caf38f87ed5626e4cb80ca2242f32
pulling-star <milanjackson426@gmail.com>
2020-01-01 21:24:56 +0530

    Intial checkins

---
+++ frontend/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default() => {
+	return (
+		<div>
+			Im some on other page!
+			<Link tp="/">Go back home</Link>
+		</div>
+	);
+}; 
